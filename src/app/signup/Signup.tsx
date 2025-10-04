eeb482964114710fd8b3b1ebf615ee7938845947
김예람 <yeram9286@naver.com>
2025-10-05 02:03:03 +0900

    프로젝트 환경설정

---
+++ src/app/signup/Signup.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import react from "react";
+import styles from "@/app/signup/Signup.module.css";
+
+const Signup = ()=> {
+
+    return (
+        <div className={styles.container}>
+            <h1>회원가입 UI 퍼블리싱을 시작할 곳입니다.</h1>
+            {/* 여기에 이제 과제 디자인을 보고 input, button 등 UI 구현 */}
+        </div>
+    );
+};
+
+export default Signup;
