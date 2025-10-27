5f371825df62f826c4abea6d39704b3e554028af
soyeoung1 <soyeoung06@gmail.com>
2025-10-27 14:58:18 +0900

    feat: 인풋란, 라디오 버튼 구현

--- src/app/signup/Signup.tsx
+++ src/app/signup/Signup.tsx
@@ -1,16 +1,67 @@
-'use client';
+"use client";
 
-import react from "react";
+import React from "react";
 import styles from "@/app/signup/Signup.module.css";
 
-const Signup = ()=> {
+const Signup = () => {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>회원가입</h2>
 
-    return (
-        <div className={styles.container}>
-            <h1>회원가입 UI 퍼블리싱을 시작할 곳입니다.</h1>
-            {/* 여기에 이제 과제 디자인을 보고 input, button 등 UI 구현 */}
+      <div className={styles.field}>
+        <div className={styles.input}>
+          <b>아이디</b>
+          <input
+            type="text"
+            placeholder="아이디를 입력해주세요."
+            className={styles.input}
+          />
         </div>
-    );
+
+        <div className={styles.input}>
+          <b>비밀번호</b>
+          <input
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+            className={styles.input}
+          />
+        </div>
+
+        <div className={styles.input}>
+          <b>비밀번호 확인</b>
+          <input
+            type="password"
+            placeholder="비밀번호를 다시 입력해주세요."
+            className={styles.input}
+          />
+        </div>
+
+        <div className={styles.input}>
+          <b>이름</b>
+          <input
+            type="text"
+            placeholder="이름을 입력해주세요."
+            className={styles.input}
+          />
+        </div>
+
+        <div className={styles.gender}>
+          <b>성별</b>
+          <div className={styles.radio}>
+            <label>
+              <input type="radio" name="gender" /> 남성
+            </label>
+            <label>
+              <input type="radio" name="gender" /> 여성
+            </label>
+            <label>
+              <input type="radio" name="gender" /> 비공개
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
